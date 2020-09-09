0cadbd8d4e49f5bc461dc9ebc835e3ae8ce1263e
SoundSlab <administrator@Mac-mini.local>
2020-09-09 11:01:52 -0400

    predeploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-console.log(process.env.DB_URL);
+console.log(process.env.DATABASE_URL);
 
 module.exports = {
     "MigrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DB_URL
-    : process.env.DB_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://chordmaster@localhost/chordtester',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chordmaster@localhost/chordtester',
     JWT_SECRET: process.env.JWT_SECRET || '12345678'
   }
